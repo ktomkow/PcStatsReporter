18f3561c92da3ec4d2715517eb9fb4c977b5d97f
ktomkow <krzysztof.tomkow@gmail.com>
2022-06-27 21:44:23 +0200

    Add client app

---
+++ client/src/pages/Index.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-page class="flex flex-center column">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    />
+    <q-btn color="primary" label="shot" @click="onClick" class="q-ma-xl" />
+  </q-page>
+</template>
+
+<script>
+import net from "net";
+import { defineComponent, onMounted } from "vue";
+
+export default defineComponent({
+  name: "PageIndex",
+  setup() {
+    onMounted(() => {
+      console.warn("Yeah ho mounted");
+    });
+
+    const onClick = () => {
+      var socket = new net.Socket();
+      socket.connect(9090, "localhost", () => {
+        console.log("Yeah");
+      });
+    };
+
+    return { onClick };
+  },
+});
+</script>
