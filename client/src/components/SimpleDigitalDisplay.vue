c12ce3bc656f60ca3f368433f74aea3a532f1a58
ktomkow <krzysztof.tomkow@gmail.com>
2022-07-03 17:38:58 +0200

    Get and display value

---
+++ client/src/components/SimpleDigitalDisplay.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div>
+      {{ displayValue }}
+    </div>
+    <div v-if="label">
+      {{ label }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { computed } from "vue";
+
+export default {
+  name: "SimpleDigitalDisplay",
+  props: {
+    value: {
+      type: Number,
+      required: false,
+      default: null,
+    },
+    unit: {
+      type: String,
+      required: false,
+      default: null,
+    },
+    label: {
+      type: String,
+      required: false,
+      default: null,
+    },
+  },
+  setup(props, { emit }) {
+    const displayValue = computed(() => {
+      if (!props.value) {
+        return "0";
+      }
+
+      if (!props.unit) {
+        return props.value.toString();
+      }
+
+      return props.value.toString() + " " + props.unit;
+    });
+
+    return { displayValue };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- client/src/pages/Index.vue
+++ client/src/pages/Index.vue
@@ -6,28 +6,57 @@
       style="width: 200px; height: 200px"
     />
     <q-btn color="primary" label="shot" @click="onClick" class="q-ma-xl" />
+    <SimpleDigitalDisplay :value="cpuAverageTemperature" />
   </q-page>
 </template>
 
 <script>
-import net from "net";
-import { defineComponent, onMounted } from "vue";
+import { reactive, toRefs, defineComponent, onMounted, onUnmounted } from "vue";
+import { api } from "src/boot/axios";
+import SimpleDigitalDisplay from "src/components/SimpleDigitalDisplay";
 
 export default defineComponent({
   name: "PageIndex",
+  components: { SimpleDigitalDisplay },
   setup() {
+    const state = reactive({ cpuAverageTemperature: 0 });
+
     onMounted(() => {
       console.warn("Yeah ho mounted");
     });
 
-    const onClick = () => {
-      var socket = new net.Socket();
-      socket.connect(9090, "localhost", () => {
-        console.log("Yeah");
-      });
+    const calculateAverage = (values) => {
+      let sum = 0;
+
+      if (!values || values.length === 0) {
+        return null;
+      }
+
+      for (let number of values) {
+        sum += number;
+      }
+
+      return sum / values.length;
     };
 
-    return { onClick };
+    const calculateAverageTemperature = (cpuData) => {
+      const coresTemperature = cpuData.cores.map((x) => x.temperature);
+
+      return calculateAverage(coresTemperature);
+    };
+
+    const onClick = async () => {
+      try {
+        const result = await api.get("api/cpu");
+        const cpuData = result.data;
+        console.log(cpuData);
+        state.cpuAverageTemperature = calculateAverageTemperature(cpuData);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    return { ...toRefs(state), onClick };
   },
 });
 </script>
