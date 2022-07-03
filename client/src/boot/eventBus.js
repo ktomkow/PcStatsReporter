58056c517628bf6f429b7183c3dfce6b590a36ad
ktomkow <krzysztof.tomkow@gmail.com>
2022-07-03 19:38:15 +0200

    Fix a bug when app does not start

--- client/src/boot/eventBus.js
+++ client/src/boot/eventBus.js
@@ -7,7 +7,6 @@ const eventBus = mitt();
 // more info on params: https://v2.quasar.dev/quasar-cli/boot-files
 export default boot(async (/* { app, router, ... } */) => {
   // something to do
-  app.config.globalProperties.$eventBus = eventBus;
 });
 
 export { eventBus };
