acac54a28ee348915171b08fa8b8436912c50ab4
ktomkow <krzysztof.tomkow@gmail.com>
2022-07-03 20:05:58 +0200

    Use statistics store in main store index file

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,6 +1,6 @@
-import { store } from 'quasar/wrappers'
-import { createStore } from 'vuex'
-
+import { store } from "quasar/wrappers";
+import { createStore } from "vuex";
+import statistics from "./statistics";
 // import example from './module-example'
 
 /*
@@ -15,13 +15,13 @@ import { createStore } from 'vuex'
 export default store(function (/* { ssrContext } */) {
   const Store = createStore({
     modules: {
-      // example
+      statistics,
     },
 
     // enable strict mode (adds overhead!)
     // for dev mode and --debug builds only
-    strict: process.env.DEBUGGING
-  })
+    strict: process.env.DEBUGGING,
+  });
 
-  return Store
-})
+  return Store;
+});
