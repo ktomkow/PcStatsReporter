ba070aedd793f0092ec3bc0217b40d757b8e0c27
ktomkow <krzysztof.tomkow@gmail.com>
2022-07-03 17:07:04 +0200

    Add healthcheck controller and remove setting to use https and iss

---
+++ src/PcStatsReporter.AspNetCore/Controllers/HealthCheckController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PcStatsReporter.AspNetCore.Controllers;
+
+[ApiController]
+[Route("api/hc")]
+public class HealthCheckController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok();
+    }
+}
