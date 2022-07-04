ed3ae2ccd6ef5fbd1affc02033c17703cf24b06c
ktomkow <krzysztof.tomkow@gmail.com>
2022-07-04 19:48:12 +0200

    Move swagger config to separate file

--- src/PcStatsReporter.AspNetCore/Program.cs
+++ src/PcStatsReporter.AspNetCore/Program.cs
@@ -1,14 +1,11 @@
-using System;
-using System.IO;
 using System.Linq;
-using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
 using PcStatsReporter.AspNetCore.Mappers;
 using PcStatsReporter.AspNetCore.Mappers.Maps;
+using PcStatsReporter.AspNetCore.ServiceProviders;
 using PcStatsReporter.Core.Models;
 using PcStatsReporter.Grpc;
 using PcStatsReporter.LibreHardware;
@@ -33,16 +30,8 @@ builder.WebHost.ConfigureKestrel(options =>
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo(){Title = "PC Hardware Monitor", Version = "v1"});
-    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    c.IncludeXmlComments(xmlPath);
-});
 
+builder.Services.AddCustomSwagger();
 
 builder.Services.AddSingleton<IMap<CpuData, CpuResponse>, CpuMap>();
 builder.Services.AddSingleton<CpuDataCollector>();
@@ -52,12 +41,7 @@ builder.Services.UseReporterGrpc();
 var app = builder.Build();
 
 
-app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-    c.RoutePrefix = "swagger";
-});
+app.UseCustomSwagger();
 
 
 app.UseRouting();
---
+++ src/PcStatsReporter.AspNetCore/ServiceProviders/SwaggerProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace PcStatsReporter.AspNetCore.ServiceProviders;
+
+public static class SwaggerProvider
+{
+    public static void AddCustomSwagger(this IServiceCollection services)
+    {
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo(){Title = "PC Hardware Monitor", Version = "v1"});
+            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            c.IncludeXmlComments(xmlPath);
+        });
+    }
+
+    public static void UseCustomSwagger(this WebApplication app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            c.RoutePrefix = "swagger";
+        });
+    }
+}
