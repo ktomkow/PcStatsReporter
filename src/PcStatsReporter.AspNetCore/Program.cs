b14b60bc2d65a152e2fe8d78e3f4d8eb26908398
ktomkow <krzysztof.tomkow@gmail.com>
2022-07-04 19:54:50 +0200

    Move kestrel configuration to separate file

--- src/PcStatsReporter.AspNetCore/Program.cs
+++ src/PcStatsReporter.AspNetCore/Program.cs
@@ -1,34 +1,14 @@
-using System.Linq;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.DependencyInjection;
-using PcStatsReporter.AspNetCore.Mappers;
-using PcStatsReporter.AspNetCore.Mappers.Maps;
 using PcStatsReporter.AspNetCore.ServiceProviders;
-using PcStatsReporter.Core.Models;
 using PcStatsReporter.Grpc;
 using PcStatsReporter.LibreHardware;
-using PcStatsReporter.RestContracts;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.ConfigureKestrel(options =>
-{
-    var portsAsStrings = builder.Configuration.GetSection("Ports").Value;
-    var ports = portsAsStrings.Split(";").Select(int.Parse).ToList();
-    
-    // Setup a HTTP/2 endpoint without TLS for grpc
-    options.ListenLocalhost(ports.First(), o => o.Protocols =
-        HttpProtocols.Http2);
-    
-    // Setup a HTTP/1 endpoint without TLS for web clients
-    options.ListenLocalhost(ports.Last(), o => o.Protocols =
-        HttpProtocols.Http1);
-});
+builder.ConfigureKestrel();
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 
 builder.Services.AddCustomSwagger();
@@ -41,9 +21,7 @@ builder.Services.UseReporterGrpc();
 
 var app = builder.Build();
 
-
 app.UseCustomSwagger();
-
 
 app.UseRouting();
 app.MapControllers();
---
+++ src/PcStatsReporter.AspNetCore/ServiceProviders/KestrelConfigurationProvider.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+namespace PcStatsReporter.AspNetCore.ServiceProviders;
+
+public static class KestrelConfigurationProvider
+{
+    public static void ConfigureKestrel(this WebApplicationBuilder builder)
+    {
+        builder.WebHost.ConfigureKestrel(options =>
+        {
+            var portsAsStrings = builder.Configuration.GetSection("Ports").Value;
+            var ports = portsAsStrings.Split(";").Select(int.Parse).ToList();
+    
+            // Setup a HTTP/2 endpoint without TLS for grpc
+            options.ListenLocalhost(ports.First(), o => o.Protocols =
+                HttpProtocols.Http2);
+    
+            // Setup a HTTP/1 endpoint without TLS for web clients
+            options.ListenLocalhost(ports.Last(), o => o.Protocols =
+                HttpProtocols.Http1);
+        });
+    }
+}
