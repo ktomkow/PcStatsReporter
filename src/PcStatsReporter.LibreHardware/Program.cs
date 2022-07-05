93aa7badba254a936720e4424ab793ce3a3da7b3
ktomkow <krzysztof.tomkow@gmail.com>
2022-07-05 20:17:50 +0200

    Add example how to get hardware data - must be run as administrator

---
+++ src/PcStatsReporter.LibreHardware/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using LibreHardwareMonitor.Hardware;
+
+namespace PcStatsReporter.LibreHardware;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Init");
+
+        var computer = new Computer
+        {
+            IsCpuEnabled = true,
+            IsGpuEnabled = true,
+            IsPsuEnabled = true,
+            IsMemoryEnabled = true,
+            IsMotherboardEnabled = true,
+            IsNetworkEnabled = true,
+            IsControllerEnabled = true,
+            IsBatteryEnabled = true,
+            IsStorageEnabled = true
+        };
+        
+        computer.Open();
+        
+        foreach (var hardware in computer.Hardware)
+        {
+            hardware.Update(); //use hardware.Name to get CPU model
+            Console.WriteLine($"Hardware: {hardware.Name} : {hardware.Identifier}");
+            foreach (var sensor in hardware.Sensors)
+            {
+                Console.WriteLine($"    {sensor.SensorType} - {sensor.Name} - {sensor.Value}");
+            }
+        }
+
+        Console.WriteLine("Finished");
+        Console.ReadLine();
+    }
+}
