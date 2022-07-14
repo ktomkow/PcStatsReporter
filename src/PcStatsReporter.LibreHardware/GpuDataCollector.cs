33bcc75bdbe8a1fd155588ab1cdd66a6895192dc
ktomkow <krzysztof.tomkow@gmail.com>
2022-07-14 21:49:53 +0200

    Add missing implementation

--- src/PcStatsReporter.LibreHardware/GpuDataCollector.cs
+++ src/PcStatsReporter.LibreHardware/GpuDataCollector.cs
@@ -20,21 +20,8 @@ public class GpuDataCollector
     
     public GpuData Collect()
     {
-        foreach (var hardware in computer.Hardware)
-        {
-            hardware.Update(); //use hardware.Name to get CPU model
+        GpuData gpu = computer.Hardware.GetGpu();
 
-            var data = hardware.Sensors.GetRam();
-            
-            GpuData gpu = new GpuData()
-            {
-                // Used = Math.Round(data.used, 2),
-                // Total = Math.Round(data.used + data.available, 2)
-            };
-
-            return gpu;
-        }
-
-        return null;
+        return gpu;
     }
 }
