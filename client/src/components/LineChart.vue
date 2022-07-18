88e92b33f363c5f68869c76258ed71127a985ede
ktomkow <krzysztof.tomkow@gmail.com>
2022-07-18 21:55:51 +0200

    Move line chart to separate component and use in temperature chart component

---
+++ client/src/components/LineChart.vue
@@ -0,0 +1,121 @@
+<template>
+  <VChart :option="options" style="width: 30em; height: 30em" />
+</template>
+
+<script>
+import { reactive, toRefs, ref } from "vue";
+import "echarts";
+import VChart from "vue-echarts";
+import { use } from "echarts";
+
+import { TitleComponent, GridComponent } from "echarts/components";
+import { LineChart } from "echarts/charts";
+import { UniversalTransition } from "echarts/features";
+import { CanvasRenderer } from "echarts/renderers";
+
+import { useEventBus } from "src/composables/eventBusComposable";
+
+use([
+  TitleComponent,
+  GridComponent,
+  LineChart,
+  CanvasRenderer,
+  UniversalTransition,
+]);
+
+export default {
+  name: "LineChart",
+  components: { VChart },
+  props: {
+    eventBusKey: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    max: {
+      type: Number,
+      required: false,
+      default: 100,
+    },
+    min: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    size: {
+      type: String,
+      required: false,
+      default: "md",
+    },
+    lineColor: {
+      type: String,
+      required: false,
+      default: "blue",
+    },
+  },
+  setup(props) {
+    const state = reactive({});
+    const myData = ref([]);
+
+    useEventBus(props.eventBusKey, addValue);
+
+    // {value, date}
+    function addValue(item) {
+      if (myData.value.length > 60) {
+        myData.value.shift();
+      }
+
+      myData.value.push({
+        name: item.date.toString(),
+        value: [item.date, item.value],
+      });
+    }
+
+    const options = ref({
+      title: {
+        text: props.title,
+      },
+      grid: {
+        left: "8%",
+        right: "4%",
+        bottom: "4%",
+        top: "8%",
+      },
+      xAxis: {
+        type: "time",
+        show: false,
+      },
+      yAxis: {
+        type: "value",
+        min: props.min,
+        max: props.max,
+        splitLine: {
+          show: true,
+        },
+      },
+      series: [
+        {
+          name: "Fake Data",
+          type: "line",
+          showSymbol: false,
+          data: myData,
+          areaStyle: {},
+          smooth: true,
+          color: props.lineColor,
+        },
+      ],
+    });
+
+    return { ...toRefs(state), options };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.md {
+  height: 10em;
+  width: 10em;
+}
+</style>
--- client/src/components/TemperatureChart.vue
+++ client/src/components/TemperatureChart.vue
@@ -1,120 +1,25 @@
 <template>
   <q-card flat bordered>
     <q-card-section>
-      <VChart :option="options" style="height: 30em; width: 30em" />
+      <LineChart :event-bus-key="eventBusKey" title="Temperature" color="red" />
     </q-card-section>
   </q-card>
 </template>
 
 <script>
-import { reactive, toRefs, computed, ref } from "vue";
-import "echarts";
-import VChart from "vue-echarts";
-import { use } from "echarts";
-
-import {
-  TitleComponent,
-  TooltipComponent,
-  GridComponent,
-} from "echarts/components";
-import { LineChart } from "echarts/charts";
-import { UniversalTransition } from "echarts/features";
-import { CanvasRenderer } from "echarts/renderers";
-
-import { useEventBus } from "src/composables/eventBusComposable";
-
-use([
-  TitleComponent,
-  TooltipComponent,
-  GridComponent,
-  LineChart,
-  CanvasRenderer,
-  UniversalTransition,
-]);
+import LineChart from "src/components/LineChart";
 
 export default {
   name: "TemperatureChart",
-  components: { VChart },
+  components: { LineChart },
   props: {
     eventBusKey: {
       type: String,
       required: true,
     },
   },
-  setup(props) {
-    const state = reactive({});
-    const myData = ref([]);
-
-    useEventBus(props.eventBusKey, addValue);
-
-    // {value, date}
-    function addValue(item) {
-      if (myData.value.length > 60) {
-        myData.value.shift();
-      }
-
-      myData.value.push({
-        name: item.date.toString(),
-        value: [item.date, item.value],
-      });
-    }
-
-    const options = ref({
-      title: {
-        text: "Dynamic Data & Time Axis",
-      },
-      grid: {
-        left: "8%",
-        right: "4%",
-        bottom: "4%",
-        top: "8%",
-      },
-      tooltip: {
-        trigger: "axis",
-        formatter: function (params) {
-          params = params[0];
-          var date = new Date(params.name);
-          return (
-            date.getDate() +
-            "." +
-            (date.getMonth() + 1) +
-            "." +
-            date.getFullYear() +
-            " - " +
-            params.value[1] +
-            "℃"
-          );
-        },
-        axisPointer: {
-          animation: false,
-        },
-      },
-      xAxis: {
-        type: "time",
-        show: false,
-      },
-      yAxis: {
-        type: "value",
-        min: 0,
-        max: 120,
-        splitLine: {
-          show: true,
-        },
-      },
-      series: [
-        {
-          name: "Fake Data",
-          type: "line",
-          showSymbol: false,
-          data: myData,
-          areaStyle: {},
-          smooth: true,
-          color: "rgb(230,0,38)",
-        },
-      ],
-    });
-
-    return { ...toRefs(state), options };
+  setup() {
+    return {};
   },
 };
 </script>
