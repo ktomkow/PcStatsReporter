67b6d5f7f3a4d3a9c3e8a95c4dc30f8515e43403
ktomkow <krzysztof.tomkow@gmail.com>
2022-07-21 20:24:27 +0200

    Bunch of new classes and interfaces

---
+++ src/PcStatsReporter.Core/ReportingClientSettings/ReportingClientSettings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace PcStatsReporter.Core.ReportingClientSettings
+{
+    public abstract class ReportingClientSettings
+    {
+        public bool Enabled { get; set; }
+        public TimeSpan Period { get; set; }
+    }
+}
