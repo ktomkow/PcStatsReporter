840abace25715ea020f4b21309ffc9525f27e3d8
ktomkow <krzysztof.tomkow@gmail.com>
2022-07-21 21:22:59 +0200

    Work on to string to debug later

---
+++ src/PcStatsReporter.Core/Models/CoreSample.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace PcStatsReporter.Core.Models
+{
+    public class CoreSample
+    {
+        public uint CoreNumber { get; set; }
+        public uint Temperature { get; set; }
+        public uint Speed { get; set; }
+        public IReadOnlyList<(uint threadNumber, uint threadLoad)> ThreadsLoad { get; set; }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            
+            return sb.ToString();
+        }
+    }
+}
---
+++ src/PcStatsReporter.Core/Models/PcInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace PcStatsReporter.Core.Models
+{
+    public class PcInfo
+    {
+        public string CpuName { get; set; }
+        public string GpuName { get; set; }
+        public double TotalRam { get; set; }
+    }
+}
