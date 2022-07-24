cf17e3dba12bdba6dd972256d85f19c16a476d32
ktomkow <krzysztof.tomkow@gmail.com>
2022-07-24 16:09:44 +0200

    Do noe use unused variable

--- src/PcStatsReporter.Client/SettingsInitializer.cs
+++ src/PcStatsReporter.Client/SettingsInitializer.cs
@@ -6,18 +6,15 @@ public class SettingsInitializer : Initializer<Settings>
 {
     private readonly AppContext _appContext;
     private readonly SettingsCollector _settingsCollector;
-    private readonly ClientChannel _clientChannel;
 
     public SettingsInitializer(ILogger<SettingsInitializer> logger, AppContext appContext, SettingsCollector settingsCollector) : base(logger)
     {
         _appContext = appContext;
         _settingsCollector = settingsCollector;
-        _clientChannel = appContext.ClientChannel;
     }
 
     protected override async Task InitializeResult(Settings initializable)
     {
-        await _clientChannel.WaitForInitialization();
         var settings = await _settingsCollector.Get();
 
         _appContext.Settings.SettingsRefreshSettings.Period = settings.GetServiceSettings().Period;
