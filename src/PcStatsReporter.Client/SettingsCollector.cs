ee0ffdadea2cd6b0d0d32b01f85d68bcf235f974
ktomkow <krzysztof.tomkow@gmail.com>
2022-07-24 15:56:45 +0200

    Work on settings initializer

--- src/PcStatsReporter.Client/SettingsCollector.cs
+++ src/PcStatsReporter.Client/SettingsCollector.cs
@@ -1,10 +1,12 @@
 ﻿using PcStatsReporter.Core.ReportingClientSettings;
+using PcStatsReporter.Grpc.Proto;
 
 namespace PcStatsReporter.Client;
 
 public class SettingsCollector
 {
     private readonly AppContext _appContext;
+    private SettingsManager.SettingsManagerClient _client;
 
     public SettingsCollector(AppContext appContext)
     {
@@ -13,6 +15,64 @@ public class SettingsCollector
     
     public async Task<List<ReportingClientSettings>> Get()
     {
-        throw new Exception();
+        await _appContext.WaitForInitialization();
+        if (_client is null)
+        {
+            await SelfInit();
+        }
+
+        SettingsRequest request = new SettingsRequest();
+        SettingsResponse response = await _client.GetAsync(request);
+
+        var list = new List<ReportingClientSettings>();
+
+        foreach (var setting in response.Settings)
+        {
+            switch (setting.Sensor)
+            {
+                case SettingType.Service:
+                    var serviceSettings = new SettingsRefreshSettings()
+                    {
+                        Period = TimeSpan.FromSeconds(setting.Period)
+                    };
+                    list.Add(serviceSettings);
+                    break;
+                
+                case SettingType.Cpu:
+                    var cpuSettings = new CpuCollectSettings()
+                    {
+                        Period = TimeSpan.FromSeconds(setting.Period)
+                    };
+                    list.Add(cpuSettings);
+                    break;
+                
+                case SettingType.Gpu:
+                    var gpuSettings = new GpuCollectSettings()
+                    {
+                        Period = TimeSpan.FromSeconds(setting.Period)
+                    };
+                    list.Add(gpuSettings);
+                    break;
+                
+                case SettingType.Ram:
+                    var ramSettings = new RamCollectSettings()
+                    {
+                        Period = TimeSpan.FromSeconds(setting.Period)
+                    };
+                    list.Add(ramSettings);
+                    break;
+                
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        
+        return list;
+    }
+
+    private async Task SelfInit()
+    {
+        await _appContext.ClientChannel.WaitForInitialization();
+        _client = new SettingsManager.SettingsManagerClient(_appContext.ClientChannel.GrpcChannel);
     }
 }
--- src/PcStatsReporter.Client/SettingsInitializer.cs
+++ src/PcStatsReporter.Client/SettingsInitializer.cs
@@ -4,16 +4,25 @@ namespace PcStatsReporter.Client;
 
 public class SettingsInitializer : Initializer<Settings>
 {
+    private readonly AppContext _appContext;
+    private readonly SettingsCollector _settingsCollector;
     private readonly ClientChannel _clientChannel;
 
-    public SettingsInitializer(ILogger<SettingsInitializer> logger, ClientChannel clientChannel) : base(logger)
+    public SettingsInitializer(ILogger<SettingsInitializer> logger, AppContext appContext, SettingsCollector settingsCollector) : base(logger)
     {
-        _clientChannel = clientChannel;
+        _appContext = appContext;
+        _settingsCollector = settingsCollector;
+        _clientChannel = appContext.ClientChannel;
     }
 
     protected override async Task InitializeResult(Settings initializable)
     {
         await _clientChannel.WaitForInitialization();
-        initializable.MarkAsInitialized() ;
+        var settings = await _settingsCollector.Get();
+
+        _appContext.Settings.SettingsRefreshSettings.Period = settings.GetServiceSettings().Period;
+        _appContext.Settings.CpuCollectSettings.Period = settings.GetCpuSettings().Period;
+        _appContext.Settings.GpuCollectSettings.Period = settings.GetGpuSettings().Period;
+        _appContext.Settings.RamCollectSettings.Period = settings.GetRamSettings().Period;
     }
 }
