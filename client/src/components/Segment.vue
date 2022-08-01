d118e2b900aa893065eaf2de662e76aef950070a
ktomkow <krzysztof.tomkow@gmail.com>
2022-08-01 19:19:05 +0200

    Segment

---
+++ client/src/components/Segment.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-card flat bordered>
+    <q-card-section>
+      <slot></slot>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: "Segment",
+};
+</script>
+
+<style lang="scss" scoped></style>
