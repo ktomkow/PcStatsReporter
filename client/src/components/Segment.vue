d3a51fbfe2dd0021a7653b4f644c61707be1d917
ktomkow <krzysztof.tomkow@gmail.com>
2022-08-01 21:03:09 +0200

    Segment with loading prop

--- client/src/components/Segment.vue
+++ client/src/components/Segment.vue
@@ -1,20 +1,74 @@
 <template>
-  <q-card flat bordered class="segment">
-    <q-card-section class="fit">
+  <q-card flat bordered :class="componentClass">
+    <q-card-section class="fit q-pa-none q-ma-none">
       <slot></slot>
     </q-card-section>
+    <InnerLoad :show="isLoading" />
   </q-card>
 </template>
 
 <script>
+import { computed } from "vue";
+import InnerLoad from "src/components/InnerLoad";
+
 export default {
   name: "Segment",
+  components: { InnerLoad },
+  props: {
+    size: {
+      type: String,
+      required: false,
+      default: "md",
+    },
+    isLoading: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  setup(props) {
+    const sizeClass = computed(() => {
+      switch (props.size) {
+        case "md":
+          return { "segment-md": true };
+        case "sm":
+          return { "segment-sm": true };
+        case "xs":
+          return { "segment-xs": true };
+        case "xxs":
+          return { "segment-xxs": true };
+        default:
+          return { "segment-md": true };
+      }
+    });
+
+    const componentClass = computed(() => {
+      return sizeClass.value;
+    });
+
+    return { componentClass };
+  },
 };
 </script>
 
-<style lang="scss" scoped>
-.segment {
+<style lang="css">
+.segment-md {
   width: 30em;
   height: 30em;
+}
+
+.segment-sm {
+  width: 20em;
+  height: 20em;
+}
+
+.segment-xs {
+  width: 15em;
+  height: 15em;
+}
+
+.segment-xxs {
+  width: 10em;
+  height: 10em;
 }
 </style>
