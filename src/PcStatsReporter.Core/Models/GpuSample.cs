d3c3f39acc962197547ca2a5794d8c85429ba2c4
ktomkow <krzysztof.tomkow@gmail.com>
2022-08-03 21:32:19 +0200

    Add GPUSample properties

--- src/PcStatsReporter.Core/Models/GpuSample.cs
+++ src/PcStatsReporter.Core/Models/GpuSample.cs
@@ -1,10 +1,34 @@
-﻿namespace PcStatsReporter.Core.Models
+﻿using System.Text;
+
+namespace PcStatsReporter.Core.Models
 {
     public class GpuSample : Sample
     {
+        public uint CoreTemperature { get; set; }
+        public uint GpuCoreClock { get; set; }
+        public uint GpuMemoryClock { get; set; }
+        public uint GpuCoreLoad { get; set; }
+        public uint GpuMemoryControllerLoad { get; set; }
+        public uint GpuVideEngineLoad { get; set; }
+        public uint GpuBusLoad { get; set; }
+        public uint GpuMemoryUsed { get; set; }
+        
         protected override string Format()
         {
-            throw new System.NotImplementedException();
+            var sb = new StringBuilder();
+            sb.AppendLine($"CoreTemperature: {CoreTemperature} C");
+            
+            sb.AppendLine($"GpuCoreClock: {GpuCoreClock} MHz");
+            sb.AppendLine($"GpuMemoryClock: {GpuMemoryClock} MHz");
+            
+            sb.AppendLine($"GpuCoreLoad: {GpuCoreLoad} %");
+            sb.AppendLine($"GpuMemoryControllerLoad: {GpuMemoryControllerLoad} %");
+            sb.AppendLine($"GpuVideEngineLoad: {GpuVideEngineLoad} %");
+            sb.AppendLine($"GpuBusLoad: {GpuBusLoad} %");
+            
+            sb.AppendLine($"GpuMemoryUsed: {GpuMemoryUsed} C");
+
+            return sb.ToString();
         }
     }
 }
