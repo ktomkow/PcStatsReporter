c446e92261c82f81bfed809fda4a32f272fab2f9
ktomkow <krzysztof.tomkow@gmail.com>
2022-08-07 21:12:31 +0200

    Ram service collector class

---
+++ src/PcStatsReporter.Client/CollectorServices/RamCollectorService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace PcStatsReporter.Client.CollectorServices;
+
+public class RamCollectorService : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            // to stuff
+            
+            await Task.Delay(TimeSpan.FromSeconds(1), stoppingToken);
+        }
+    }
+}
