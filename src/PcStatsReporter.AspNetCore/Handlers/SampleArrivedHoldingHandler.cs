0911b9b0a1d56b6cca4de168d748a6a05c91901a
ktomkow <krzysztof.tomkow@gmail.com>
2022-08-09 21:20:13 +0200

    Add handler that sets latest sample

---
+++ src/PcStatsReporter.AspNetCore/Handlers/SampleArrivedHoldingHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using PcStatsReporter.AspNetCore.Messages;
+using PcStatsReporter.Core.Persistence;
+using Rebus.Handlers;
+
+namespace PcStatsReporter.AspNetCore.Handlers;
+
+public class SampleArrivedHoldingHandler : IHandleMessages<CpuSampleArrivedEvent>, IHandleMessages<GpuSampleArrivedEvent>, IHandleMessages<RamSampleArrivedEvent>
+{
+    private readonly ILogger<SampleArrivedHoldingHandler> _logger;
+    private readonly IHold _holder;
+
+    public SampleArrivedHoldingHandler(ILogger<SampleArrivedHoldingHandler> logger, IHold holder)
+    {
+        _logger = logger;
+        _holder = holder;
+    }
+    
+    public async Task Handle(CpuSampleArrivedEvent message)
+    {
+        _logger.LogDebug("Setting latest {Type}", message.CpuSample.GetType().Name);
+        await _holder.Set(message.CpuSample);
+    }
+
+    public async Task Handle(GpuSampleArrivedEvent message)
+    {
+        _logger.LogDebug("Setting latest {Type}", message.GpuSample.GetType().Name);
+        await _holder.Set(message.GpuSample);
+    }
+
+    public async Task Handle(RamSampleArrivedEvent message)
+    {
+        _logger.LogDebug("Setting latest {Type}", message.RamSample.GetType().Name);
+        await _holder.Set(message.RamSample);
+    }
+}
