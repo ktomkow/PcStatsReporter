7e01a354bb28f10091226500b654657d89eafafe
ktomkow <krzysztof.tomkow@gmail.com>
2022-08-09 20:51:40 +0200

    Default 10 seconds of waiting

--- src/PcStatsReporter.AspNetCore/Program.cs
+++ src/PcStatsReporter.AspNetCore/Program.cs
@@ -33,7 +33,7 @@ builder.Services.AddServerMaps();
 
 DefaultSetting defaultSetting = new DefaultSetting()
 {
-    Period = TimeSpan.FromSeconds(1)
+    Period = TimeSpan.FromSeconds(10)
 };
 
 builder.Services.AddSingleton(defaultSetting);
