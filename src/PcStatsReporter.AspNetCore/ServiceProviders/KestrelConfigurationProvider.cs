94b2c7792ece87fddfc95fe253d29e4b96291768
ktomkow <krzysztof.tomkow@gmail.com>
2022-08-14 21:18:38 +0200

    Reconfigure ports to work on server properly

--- src/PcStatsReporter.AspNetCore/ServiceProviders/KestrelConfigurationProvider.cs
+++ src/PcStatsReporter.AspNetCore/ServiceProviders/KestrelConfigurationProvider.cs
@@ -14,12 +14,12 @@ public static class KestrelConfigurationProvider
             var portsAsStrings = builder.Configuration.GetSection("Ports").Value;
             var ports = portsAsStrings.Split(";").Select(int.Parse).ToList();
     
-            // Setup a HTTP/2 endpoint without TLS for grpc
-            options.ListenLocalhost(ports.First(), o => o.Protocols =
+            // Setup a HTTP/1 endpoint without TLS for grpc
+            options.ListenAnyIP(ports.First(), o => o.Protocols =
                 HttpProtocols.Http1);
     
-            // Setup a HTTP/1 endpoint without TLS for web clients
-            options.ListenLocalhost(ports.Last(), o => o.Protocols =
+            // Setup a HTTP/2 endpoint without TLS for web clients
+            options.ListenAnyIP(ports.Last(), o => o.Protocols =
                 HttpProtocols.Http2);
         });
     }
