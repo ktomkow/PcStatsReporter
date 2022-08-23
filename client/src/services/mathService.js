19bc1a1558b074eb60c6cdf266acd215c5b3aa80
ktomkow <krzysztof.tomkow@gmail.com>
2022-08-23 21:16:24 +0200

    Check if number and digits in proper format

--- client/src/services/mathService.js
+++ client/src/services/mathService.js
@@ -1,7 +1,19 @@
 // todo: add tests
 
 export function roundToString(number, digits) {
-  if (!number || typeof number !== "number") {
+  if (number !== 0) {
+    if (!number || typeof number !== "number") {
+      return "";
+    }
+  }
+
+  if (digits !== 0) {
+    if (!digits || typeof digits !== "number" || digits < 0) {
+      return "";
+    }
+  }
+
+  if (Number.isInteger(digits) === false) {
     return "";
   }
 
--- client/test/jest/__tests__/mathService/mathService.roundToString.spec.js
+++ client/test/jest/__tests__/mathService/mathService.roundToString.spec.js
@@ -2,84 +2,71 @@ import { describe, expect, it } from "@jest/globals";
 
 import { roundToString } from "src/services/mathService";
 
-describe("roundToString null or other strange stuff", () => {
-  it("When input is null then empty string", () => {
-    const input = null;
-    const result = roundToString(input);
-    expect(result).toBe("");
+describe("roundToString input null or other strange stuff", () => {
+  const cases = [
+    [null],
+    [undefined],
+    [NaN],
+    [[]],
+    [[123]],
+    ["5"],
+    ["text"],
+    [() => {}],
+    [() => 5],
+    ["🍕"],
+  ];
+
+  test.each(cases)("When input %p then ''", (input) => {
+    const result = roundToString(input, 0);
+
+    expect(result).toEqual("");
+    expect(typeof result).toEqual("string");
   });
+});
 
-  it("When input is undefined then empty string", () => {
-    const input = undefined;
-    const result = roundToString(input);
-    expect(result).toBe("");
-  });
+describe("roundToString digits null or other strange stuff", () => {
+  const cases = [
+    [-211],
+    [-1],
+    [1.2],
+    [null],
+    [undefined],
+    [NaN],
+    [[]],
+    [[123]],
+    ["5"],
+    ["text"],
+    [() => {}],
+    [() => 5],
+    ["🍕"],
+  ];
+  test.each(cases)("When digits %p then ''", (digits) => {
+    const result = roundToString(123, digits);
 
-  it("When input is empty array then empty string", () => {
-    const input = [];
-    const result = roundToString(input);
-    expect(result).toBe("");
-  });
-
-  it("When input is array then empty string", () => {
-    const input = [1, 2, 3];
-    const result = roundToString(input);
-    expect(result).toBe("");
-  });
-
-  it("When input is number but a string then empty string", () => {
-    const input = "5";
-    const result = roundToString(input);
-    expect(result).toBe("");
-  });
-
-  it("When input is string then empty string", () => {
-    const input = "text";
-    const result = roundToString(input);
-    expect(result).toBe("");
-  });
-
-  it("When input is emoji then empty string", () => {
-    const input = "🍕";
-    const result = roundToString(input);
-    expect(result).toBe("");
-  });
-
-  it("When input is NaN then empty string", () => {
-    const input = NaN;
-    const result = roundToString(input);
-    expect(result).toBe("");
-  });
-
-  it("When input is function then empty string", () => {
-    const input = () => {};
-    const result = roundToString(input);
-    expect(result).toBe("");
-  });
-
-  it("When input is function that return number then empty string", () => {
-    const input = () => {
-      return 5;
-    };
-
-    const result = roundToString(input);
-    expect(result).toBe("");
+    expect(result).toEqual("");
+    expect(typeof result).toEqual("string");
   });
 });
 
 describe("roundToString to int", () => {
-  const roundToIntCases = [
+  const cases = [
     [10, "10"],
     [-23, "-23"],
+    [-23.1, "-23"],
+    [-23.5, "-24"],
+    [-23.9, "-24"],
     [1.1, "1"],
     [1.5, "2"],
     [1.9, "2"],
   ];
 
-  test.each(roundToIntCases)("When %p then %p", (input, expectedResult) => {
-    const result = roundToString(input, 0);
+  test.each(cases)(
+    "When input %p and digits 0 then %p",
+    (input, expectedResult) => {
+      const result = roundToString(input, 0);
 
-    expect(result).toEqual(expectedResult);
-    expect(typeof result).toEqual("string");
-  });
+      expect(result).toEqual(expectedResult);
+      expect(typeof result).toEqual("string");
+    }
+  );
 });
