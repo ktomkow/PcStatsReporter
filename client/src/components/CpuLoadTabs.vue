2359d44499fbd5180e2e4f74c382a1b736f4485f
ktomkow <krzysztof.tomkow@gmail.com>
2022-08-27 17:23:36 +0200

    Experiments how to dinamically add chart

--- client/src/components/CpuLoadTabs.vue
+++ client/src/components/CpuLoadTabs.vue
@@ -1,8 +1,8 @@
 <template>
   <Segment :is-loading="isLoading">
-    <div class="griid bg-orange-1 fit q-pa-md">
+    <div v-if="!isLoading" class="griid bg-orange-1 fit q-pa-md">
       <LineChart
-        v-for="i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]"
+        v-for="i in ids"
         :key="i"
         :event-bus-key="key"
         :title="'    #' + i"
@@ -33,14 +33,31 @@ export default {
     const router = useRouter();
     const q = useQuasar();
 
-    const isLoading = computed(() => true);
+    const isLoading = computed(() => ids.value.length < 1);
 
     const key = "key";
 
     let id = null;
 
+    const ids = ref([]);
+    let j = 0;
+
     onMounted(() => {
+      setTimeout(() => {
+        ids.value = [1, 2, 3, 4];
+      }, 3000);
+      setTimeout(() => {
+        ids.value = [];
+        setTimeout(() => {
+          ids.value = [1, 2, 3, 4];
+          ids.value.push(5);
+          ids.value.push(6);
+          ids.value.push(7);
+          ids.value.push(8);
+        }, 0);
+      }, 6000);
       id = setInterval(() => {
+        console.log("🚀 ~ file: CpuLoadTabs.vue ~ line 48 ~ setup ~ j", j);
         eventBus.emit(key, {
           value: Math.random() * 20 + 50,
           date: new Date(),
@@ -52,7 +69,7 @@ export default {
       clearInterval(id);
     });
 
-    return { ...toRefs(state), key, isLoading };
+    return { ...toRefs(state), key, isLoading, ids };
   },
 };
 </script>
@@ -60,7 +77,7 @@ export default {
 <style lang="scss" scoped>
 .griid {
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr;
   grid-auto-rows: 1fr;
   grid-gap: 1em;
   grid-auto-flow: row;
