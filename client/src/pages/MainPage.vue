1289b72d2815ddcb4195a342c569437be2e41b7a
ktomkow <krzysztof.tomkow@gmail.com>
2022-08-27 16:02:20 +0200

    Use 5 columns

--- client/src/pages/MainPage.vue
+++ client/src/pages/MainPage.vue
@@ -90,7 +90,7 @@ export default {
 <style lang="scss" scoped>
 .cards-grid {
   display: grid;
-  grid-template-columns: auto auto auto auto;
+  grid-template-columns: auto auto auto auto auto;
   grid-template-rows: auto auto;
   grid-gap: 1em;
   grid-auto-flow: column;
