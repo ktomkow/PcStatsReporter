0824807a6e5b863274c98283d4deaddf252d3467
ktomkow <krzysztof.tomkow@gmail.com>
2022-10-09 21:54:22 +0200

    Place ram and cpu buttons on right drawer

--- client/src/components/themes/ThemesDrawer.vue
+++ client/src/components/themes/ThemesDrawer.vue
@@ -1,22 +1,28 @@
 <template>
-  <div class="fit bg-blue-2"></div>
+  <div class="fit q-pa-sm q-gutter-sm">
+    <ColorPickButton :color-id="ColorId.RamUsageLine" label="Ram" />
+    <ColorPickButton :color-id="ColorId.CpuTempLine" label="Cpu" />
+  </div>
 </template>
 
-<script>
+<script lang="ts">
 import { reactive, toRefs, computed } from "vue";
 import { useStore } from "vuex";
 import { useQuasar } from "quasar";
 import { useRouter } from "vue-router";
 
+import ColorPickButton from "src/components/themes/ColorPickButton.vue";
+import ColorId from "src/models/Colors/ColorId";
+
 export default {
   name: "ThemesDrawer",
-  components: {},
-  setup(props) {
+  components: { ColorPickButton },
+  setup() {
     const state = reactive({});
     const store = useStore();
     const router = useRouter();
     const q = useQuasar();
-    return { ...toRefs(state) };
+    return { ...toRefs(state), ColorId };
   },
 };
 </script>
