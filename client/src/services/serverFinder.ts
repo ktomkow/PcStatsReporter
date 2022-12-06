9ca7f8c71c218eac175cfee06d322096c80eb543
ktomkow <krzysztof.tomkow@gmail.com>
2022-12-06 21:19:16 +0100

    little refactor

--- client/src/services/serverFinder.ts
+++ client/src/services/serverFinder.ts
@@ -9,20 +9,25 @@ export async function find(): Promise<Result> {
   const addressBase = "http://192.168.0.";
   const port = 11111;
 
-  console.log("lets go");
   // not very good approach so far
   for (let i = 0; i < 255; i++) {
     const address = addressBase + i + ":" + port;
-    try {
-      await axios.get(address, { timeout: 100 });
-
-      console.log("Success", address);
-      return { success: true, address: address };
-      // eslint-disable-next-line no-empty
-    } catch (e) {
-      console.debug(address, e);
+    const checkResult: Result = await check(address);
+    if (checkResult.success) {
+      return checkResult;
     }
   }
 
   return { success: false };
+}
+
+async function check(address: string): Promise<Result> {
+  try {
+    await axios.get(address + "/api/hc", { timeout: 100 });
+
+    return { success: true, address: address };
+    // eslint-disable-next-line no-empty
+  } catch (e) {
+    return { success: false };
+  }
 }
