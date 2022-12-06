96b3845bfc4fc50a32b3b7713bff71662f7c90e8
ktomkow <krzysztof.tomkow@gmail.com>
2022-12-06 21:31:05 +0100

    Modify axios to use server finder

---
+++ client/src/boot/axios.ts
@@ -0,0 +1,41 @@
+import { boot } from "quasar/wrappers";
+import axios, { AxiosInstance } from "axios";
+import { find } from "src/services/serverFinder";
+
+export interface Api {
+  isInitialized: boolean;
+  axios?: AxiosInstance;
+  baseAddress?: string;
+}
+
+// Be careful when using SSR for cross-request state pollution
+// due to creating a Singleton instance here;
+// If any client changes this (global) instance, it might be a
+// good idea to move this instance creation inside of the
+// "export default () => {}" function below (which runs individually
+// for each client)
+// const api = axios.create({ baseURL: "http://localhost:11111" });
+const api: Api = {
+  isInitialized: false,
+};
+
+export default boot(async () => {
+  setTimeout(async () => {
+    const baseAddressScanResult = await find();
+    if (baseAddressScanResult.success === false) {
+      throw "Not found base address!";
+    }
+
+    console.log("API address found", baseAddressScanResult);
+
+    const axiosInstance = axios.create({
+      baseURL: baseAddressScanResult.address,
+    });
+
+    api.axios = axiosInstance;
+    api.isInitialized = true;
+    api.baseAddress = baseAddressScanResult.address;
+  }, 0);
+});
+
+export { api };
